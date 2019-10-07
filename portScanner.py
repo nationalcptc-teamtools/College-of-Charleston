098e0916e343f8a19e450323a4aa0102c0e21abe
patmccabe5 <36641500+patmccabe5@users.noreply.github.com>
2019-10-06 21:39:12 -0400

    Update portScanner.py

--- portScanner.py
+++ portScanner.py
@@ -1,21 +1,18 @@
+#!/usr/bin/python3
 import socket
 ports = []
 numports = 0
-for i in range(1, 1025):
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(1)
+host = input("Enter the host: ")
+maxport = int(input("Enter the max port to scan: "))
+for i in range(1, maxport + 1):
     ports.append(i)
-numhosts = int(input("Enter num hosts: "))
-for i in range(numhosts):
-    ip = input("Enter ip: ")
-for port in ports:
-    print('Scanning port #' + str(port))
-    socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(1)
-    result = socket_obj.connect_ex((ip, port))
-    socket_obj.close()
-    if result == 0:
-        print('Port ' + str(port) + ' is open')
-        numPorts += 1
-    
-    if numPorts == 0:
-        print('All ports closed or firewall enabled!')
-    numports = 0
+def portScanner():
+    for port in ports:
+        if s.connect_ex((host, port)):
+            print("Port " + str(port) + " is closed")
+        else:
+            print("Port " + str(port) + " is open")
+        s.close()
+portScanner()
